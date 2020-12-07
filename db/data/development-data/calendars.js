e535b1f02c106e4f1b0e1afd741f60f3ba8caec5
AVSheard <a_sheard@yahoo.co.uk>
2020-12-07 09:55:11 +0000

    made development data for calendars

---
+++ db/data/development-data/calendars.js
@@ -0,0 +1,14 @@
+module.exports = [
+	{
+		name: "Uppermill Advent",
+		centreName: "Uppermill Methodist Church",
+		longLocation: 53.5488627,
+		latLocation: -2.0042356,
+	},
+	{
+		name: "Greenfield Advent",
+		centreName: "St Mary, Greenfield",
+		longLocation: 53.5330656,
+		latLocation: -1.9983334,
+	},
+];
