eecc1c4395ac75944669833ee79ae282a5c1d65a
AVSheard <a_sheard@yahoo.co.uk>
2020-12-21 10:49:07 +0000

    created test data

---
+++ db/data/test-data/calendars.js
@@ -0,0 +1,14 @@
+module.exports = [
+	{
+		name: "Tunstead Advent",
+		centreName: "Prospecton, Tunstead Lane",
+		longLocation: 53.53861,
+		latLocation: -1.996911,
+	},
+	{
+		name: "Greenfield Advent",
+		centreName: "St Mary, Greenfield",
+		longLocation: 53.5330656,
+		latLocation: -1.9983334,
+	},
+];
