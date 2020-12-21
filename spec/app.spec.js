d9581ecaf6df9ca95ecca3c813459ed2492913bb
AVSheard <a_sheard@yahoo.co.uk>
2020-12-21 16:40:28 +0000

    wrote test calendars get request

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -8,4 +8,23 @@ const connection = require("../db/connection");
 describe("/api", () => {
 	beforeEach(() => connection.seed.run());
 	after(() => connection.destroy());
+
+	describe("/calendars", () => {
+		it("GET - 200 for successful request for list of calendars", () => {
+			return request(app)
+				.get("/api/calendars")
+				.expect(200)
+				.then((res) => {
+					res.body.calendars.forEach((calendar) => {
+						expect(calendar).to.have.all.keys([
+							"calendarName",
+							"centreName",
+							"admin",
+							"longLocation",
+							"latLocation",
+						]);
+					});
+				});
+		});
+	});
 });
