6675a7ec70216d26414351a12efeb5def72db3a4
AVSheard <a_sheard@yahoo.co.uk>
2020-12-24 10:30:09 +0000

    changed formatiing of test folder

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,9 +1,10 @@
-process.env.NODE_ENV = "test";
 const chai = require("chai");
 const { expect } = chai;
 const request = require("supertest");
 const app = require("../app");
 const connection = require("../db/connection");
+
+process.env.NODE_ENV = "test";
 
 describe("/api", () => {
 	beforeEach(() => connection.seed.run());
